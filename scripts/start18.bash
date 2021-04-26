0ff9290c78a9dd1ce4a7e7afa15f2c5e7634195d
brucechanjianle <jianle001@e.ntu.edu.sg>
2021-04-26 10:16:00 +0800

    update start18 script

--- scripts/start18.bash
+++ scripts/start18.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
 # This script start the docker container running
 
-echo -e "Starting container image ubuntu16.04:basic"
+echo -e "Starting container image ubuntu18.04:basic"
 read -p "Container name: " CONTAINERNAME
 
-docker run -d -ti -v $(pwd)/..:/root/docker_mount --net=host --name $CONTAINERNAME --cap-add=SYS_PTRACE ubuntu16.04:basic
+docker run -d -ti \
+    -v $(pwd)/..:/root/docker_mount \
+    --net=host \
+    --name $CONTAINERNAME \
+    --cap-add=SYS_PTRACE \
+    ubuntu18.04:basic
